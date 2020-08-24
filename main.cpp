73f0d41b8e34d52820ba8b55c0f8bb38c755c28d
Melvin Diaz <melvindiaz99@gmail.com>
2020-08-24 16:11:12 -0600

    Feat: Create menus and add headers

---
+++ main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include "Process.h"
+
+using namespace std;
+
+Node *front = nullptr, *rear = nullptr;
+void menu();
+void processMenu();
+//void mmuMenu();
+
+int main() {
+    int option;
+    do{
+        menu();
+        cin >> option;
+        cout << endl;
+        switch (option) {
+            case 1:
+                processMenu();
+                break;
+            case 2:
+                cout << "Beta phase" << endl;
+                break;
+            default:
+                cout << option << ": in not a valid option" << endl << endl;
+        }
+    }while (option < 3);
+    cout << "Exiting program" << endl;
+    return 0;
+}
+
+void menu(){
+    cout << "1. Process settings \n2. MMU settings \n3. Exit \nSelect:" ;
+}
+
+void processMenu(){
+    int option;
+    cout << "1. Add a new process  \n2. Show all process \n3. Go back \nSelect:";
+    cin  >> option;
+    cout << endl;
+    switch (option) {
+        case 1:
+            push(&front,&rear);
+            break;
+        case 2:
+            show(front);
+            break;
+        default:
+            cout << option << ": in not a valid option" << endl;
+    }
+}
